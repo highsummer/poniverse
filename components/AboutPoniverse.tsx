2d1d4d9f9b4293d912573978fe6cd74905a8ebc9
Yoonha Hwang <yoonha.highsummer@gmail.com>
2022-02-09 16:36:19 +0900

    Fix small typo

--- components/AboutPoniverse.tsx
+++ components/AboutPoniverse.tsx
@@ -4,7 +4,7 @@ const AboutPoniverse: React.FunctionComponent = props => {
   return <div className={"text-justify"}>
     <strong className={"text-xl"}>포니버스에 대해</strong><br/>
     <hr className={"mb-4"}/>
-    새내기새로배움터를 주제로 꾸며진 공간 안에서 친구들을 만나보세요! 날짜별로 열리는 여러가지 콘텐츠들도 놓치지 마시길 바랍니다~
+    새내기새로배움터를 주제로 꾸며진 포니버스 안에서 친구들을 만나보세요! 날짜별로 열리는 여러가지 콘텐츠들도 놓치지 마시길 바랍니다~
   </div>
 }
 
