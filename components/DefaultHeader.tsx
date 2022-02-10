d4ae344634606080c912e0e3338d7e37c6038834
Yoonha Hwang <yoonha.highsummer@gmail.com>
2022-02-10 16:50:12 +0900

    Add opengraph metadata

--- components/DefaultHeader.tsx
+++ components/DefaultHeader.tsx
@@ -11,7 +11,22 @@ const DefaultHeader: React.FunctionComponent = props => {
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#92bf5f" />
       <meta name="msapplication-TileColor" content="#2d89ef" />
       <meta name="theme-color" content="#ffffff" />
+      
       <title>포니버스</title>
+      <meta name="description" content="새내기새로배움터를 테마로 꾸며진 세상에서 포스텍 친구들은 만나보세요!" />
+
+      <meta property="og:url" content="https://poniverse.yoonha.dev/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="포니버스" />
+      <meta property="og:description" content="새내기새로배움터를 테마로 꾸며진 세상에서 포스텍 친구들은 만나보세요!" />
+      <meta property="og:image" content="/assets/thumbnail.png" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="poniverse.yoonha.dev" />
+      <meta property="twitter:url" content="https://poniverse.yoonha.dev/" />
+      <meta name="twitter:title" content="포니버스" />
+      <meta name="twitter:description" content="새내기새로배움터를 테마로 꾸며진 세상에서 포스텍 친구들은 만나보세요!" />
+      <meta name="twitter:image" content="/assets/thumbnail.png" />
     </Head>
     {props.children}
   </React.Fragment>
